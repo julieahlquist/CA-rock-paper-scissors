7a3048446c2a95b860d85881d12ee59d009f66e8
Julie Ahlquist <julieahlquist@hotmail.com>
2019-07-05 23:12:47 +0200

    first feature test, also adds helpers in world, modifies head title and adds README

---
+++ features/main.feature
@@ -0,0 +1,12 @@
+Feature: Create game
+  As a player
+  In order to have fun
+  I would like to play the game of rock, paper, scissors
+
+
+  Scenario: Play the game against a computer
+    Given I visit the site
+    # Then I should see "Rock"
+    # And I should see "Paper"
+    # And I should see "Scissors"
+    
--- features/step_definitions/basic-steps.js
+++ features/step_definitions/basic-steps.js
@@ -1,11 +1,10 @@
 const { Given, Then } = require("cucumber");
 
-Given("I visit the site", function() {
-  // Write code here that turns the phrase above into concrete actions
-  return "pending";
-});
+Given('I visit the site', async function() {
+  return await this.openHomePage()
+})
 
-Then("I should see {string}", function(string) {
-  // Write code here that turns the phrase above into concrete actions
-  return "pending";
-});
+// Then("I should see {string}", function(string) {
+//   // Write code here that turns the phrase above into concrete actions
+//   return "pending";
+// });
