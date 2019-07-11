045e8d005a2c6f85c3bb5348132c69d9ed4c8e3f
Julie Ahlquist <julieahlquist@hotmail.com>
2019-07-11 09:00:48 +0200

    WIP, modifies to radio buttons and submit button instead

--- src/app.js
+++ src/app.js
@@ -1,6 +1,13 @@
-document.getElementById("rock").addEventListener("click", function () {
-    console.log('you clicked rock')
-})
+// document.getElementById("rock").onclick = playerThrowsRock;
+// document.getElementById("scissors").onclick = playerThrowsScissors;
+// document.getElementById("paper").onclick = playerThrowsPaper;
+
+
+
+
+// document.getElementById("rock").addEventListener("click", function () {
+//     console.log('you clicked rock')
+// })
 
 // document.getElementById("paper").addEventListener("click", function () {
 //     console.log('you clicked paper')
