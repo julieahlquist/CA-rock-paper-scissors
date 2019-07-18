3926c5c895a2476b6c889de96d3991d9a6923abb
Julie Ahlquist <julieahlquist@hotmail.com>
2019-07-18 13:38:18 +0200

    adds statement to iterate over player values

--- src/app.js
+++ src/app.js
@@ -4,56 +4,58 @@ document.addEventListener('DOMContentLoaded', () => {
     button.addEventListener('click', () => {
       let player1form = document.getElementById('player1form').value
       let player2form = document.getElementById('player2form').value
-      let play = new Play
-        let result = play.check(player1form, player2form)
+        let result = play(player1choice, player2choice)
         displayDiv.innerHTML = result; 
     })
   })
 
-function Play() {
-    this.check = (player_1, player_2) => {
-        if (player_1 === player_2) {
+var player1choice = "none"
+var player2choice = "none"
+
+function play(player1choice, player2choice) {
+        if (player1choice === player2choice) {
             return 'Tie! Play again guys!';
-        }
-
-        else if ((player_1 === "rock") && (player_2 === "paper")) {
+        }   else if ((player1choice === "rock") && (player2choice === "paper")) {
             return 'Player 2 wins!';
-        }
-
-        else if ((player_1 === "scissors") && (player_2 === "paper")) {
+        }   else if ((player1choice=== "scissors") && (player2choice === "paper")) {
             return 'Player 1 wins!';
-        }
-
-        else if ((player_1 === "scissors") && (player_2 === "rock")) {
+        }   else if ((player1choice === "scissors") && (player2choice === "rock")) {
             return 'Player 2 wins!';
-        }
-
-        else if ((player_1 === "paper") && (player_2 === "rock")) {
+        }   else if ((player1choice === "paper") && (player2choice === "rock")) {
             return 'Player 1 wins!';
-        }
-
-        else if ((player_1 === "paper") && (player_2 === "scissors")) {
+        }   else if ((player1choice === "paper") && (player2choice === "scissors")) {
             return 'Player 2 wins!';
-        }
-
-        else if ((player_1 === "rock") && (player_2 === "scissors")) {
+        }   else if ((player1choice === "rock") && (player2choice === "scissors")) {
             return 'Player 1 wins!';
-        }
-
-        else {
+        }   else {
             return 'No throws guys... try again!';
         }
-    }
 }
 
 let hide1Btn = document.getElementById("hide1")
 hide1Btn.addEventListener("click", () => {
     document.getElementById("player1form").style.display = "none";
     document.getElementById("hide1").innerHTML = "Is it a winning move?";
+
+    if(document.getElementById('p1rock').checked) { 
+        player1choice = "rock"
+    } else if (document.getElementById('p1paper').checked) {
+        player1choice = "paper"
+    } else if (document.getElementById('p1scissors').checked) {
+        player1choice = "scissors"
+    }
 });
 
 let hide2Btn = document.getElementById("hide2")
 hide2Btn.addEventListener("click", () => {
     document.getElementById("player2form").style.display = "none";
     document.getElementById("hide2").innerHTML = "Time to hit Play!";
+
+    if(document.getElementById('p2rock').checked) { 
+        player2choice = "rock"
+    } else if (document.getElementById('p2paper').checked) {
+        player2choice = "paper"
+    } else if (document.getElementById('p2scissors').checked) {
+        player2choice = "scissors"
+    }
 })
